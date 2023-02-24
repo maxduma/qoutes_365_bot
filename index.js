165b148c918fad13c0cf5738931332fe31d40c79
Max <maxduma16@gmail.com>
2023-02-24 14:54:13 +0200

    added all quotes and refactoring getQuoteForToday

--- index.js
+++ index.js
@@ -4,51 +4,57 @@ const text = require('./const');
 const data = require('./data');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-
 const getQuoteForToday = () => {
   const now = new Date();
   const dayOfYear = Math.floor((now - new Date(now.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-  const quote = data.quotes[2];
+  let indexQuote;
+  if(dayOfYear > data.quotes.length) {
+    const index = dayOfYear - data.quotes.length - 1;
+    indexQuote = data.quotes[index];
+  } else {
+    indexQuote = data.quotes[dayOfYear - 1];
+  }
+  const quote = data.quotes[indexQuote];
   return quote;
 }
 
 const showQuote = (ctx, quoteObj) => {
   if (quoteObj.author) {
-    return ctx.replyWithHTML(`<b>"${quoteObj.quote}" - ${quoteObj.author}</b>`)
+    return ctx.replyWithHTML(`<b>"${quoteObj.quote}" - ${quoteObj.author}</b>`);
   }
-  return ctx.replyWithHTML(`<b>"${quoteObj.quote}"</b>`)
+  return ctx.replyWithHTML(`<b>"${quoteObj.quote}"</b>`);
 }
 
 const getRandomNumberQuote = () => {
-  return Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * data.quotes.length);
 }
 
 bot.start(async (ctx) => {
   try {
-    await ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'stranger'}!`)
+    await ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'stranger'}!`);
 
     await setInterval(() => {
-      const quoteObj = getQuoteForToday()
+      const quoteObj = getQuoteForToday();
       showQuote(ctx, quoteObj);
     }, 10000)
     // 86400000
   } catch (error) {
-      console.error(error)
+      console.error(error);
   }
 });
 
 bot.help((ctx) => {
   try {
-    ctx.reply(text.commands)
+    ctx.reply(text.commands);
   } catch (error) {
-      console.error(error)
+      console.error(error);
   }
 });
 
 bot.command('showAllQuotes', (ctx) => {
   try {
     data.quotes.forEach((quoteObj) => {
-      showQuote(ctx, quoteObj)
+      showQuote(ctx, quoteObj);
     });
   } catch (error) {
       console.error(error);
@@ -57,10 +63,10 @@ bot.command('showAllQuotes', (ctx) => {
 
 bot.command('quoteForToday', (ctx) => {
   try {
-    const quoteObj = getQuoteForToday()
+    const quoteObj = getQuoteForToday();
     showQuote(ctx, quoteObj);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 })
 
@@ -70,13 +76,13 @@ bot.command('showRandomQuote', (ctx) => {
     const quoteObj = data.quotes[num];
     showQuote(ctx, quoteObj);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 })
 
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears(['hi', 'Hey', 'hey', 'Hi', 'Hello', 'hello', 'What is up?'], (ctx) => ctx.reply('Hey there'));
-bot.catch(console.error)
+bot.catch(console.error);
 bot.launch();
 
 // Enable graceful stop
